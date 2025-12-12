0b417f93c23e35542be7219d6c92beb6efc7ebee
phamhieus <hieupb@proton.me>
2025-12-12 16:48:07 +0700

    add view for video element

--- UnoApp.VideoGalery/Presentation/NavtiveVideoViewModel.cs
+++ UnoApp.VideoGalery/Presentation/NavtiveVideoViewModel.cs
@@ -3,19 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
+using Windows.Media.Core;
+using Windows.Storage.Pickers;
 
 namespace UnoApp.VideoGalery.Presentation;
 internal partial class NavtiveVideoViewModel : ObservableObject
 {
     [ObservableProperty]
-    private string _videoSource = "ms-appx:///Assets/Videos/sample.mp4";
+    private MediaSource _videoSource;
 
     public NavtiveVideoViewModel()
     {
+        VideoSource = MediaSource.CreateFromUri(new Uri ("ms-appx:///Assets/Videos/sample.mp4"));
     }
 
-    private void ShowPopupLoadVideo()
+    [RelayCommand]
+    private async Task ShowPopupLoadVideo()
     {
-        // Logic to load or prepare the video can be added here
+        var picker = new FileOpenPicker
+        {
+            SuggestedStartLocation = PickerLocationId.VideosLibrary
+        };
+
+        picker.FileTypeFilter.Add(".mp4");
+        picker.FileTypeFilter.Add(".avi");
+        picker.FileTypeFilter.Add(".mkv");
+        picker.FileTypeFilter.Add(".mov");
+        picker.FileTypeFilter.Add(".wmv");
+
+        var file = await picker.PickSingleFileAsync();
+        if (file is null) return;
+
+        var copied = await file.CopyAsync(
+            ApplicationData.Current.LocalFolder,
+            file.Name,
+            NameCollisionOption.GenerateUniqueName);
+
+        var uri = new Uri($"ms-appdata:///local/{copied.Name}");
+
+        VideoSource = MediaSource.CreateFromUri(uri);
     }
 }
